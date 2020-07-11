39df9dae9346446592a5fb8cabcb800d858dc530
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-07-11 16:47:40 -0300

    export component

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Icon } from './components/Icon';
 export { default as Card } from './components/Card';
 export { default as CarouselCard } from './components/CarouselCard';
+export { default as TextLogo } from './components/Logo';
