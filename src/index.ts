dc984a8762d83b0aeea74b2c33fa5b1502021e14
Eduardo Henrique <eduardo.henrique@zup.com.br>
2020-07-12 10:06:03 -0300

    export navbar

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Icon } from './components/Icon';
 export { default as Card } from './components/Card';
 export { default as CarouselCard } from './components/CarouselCard';
 export { default as TextLogo } from './components/Logo';
+export { default as Navbar } from './components/Navbar';
